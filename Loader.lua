e0625c5049512166e3c2c9d3a74dbe02632e48bf
G-MX2 <127357342+SoyAdriYT@users.noreply.github.com>
2024-09-17 21:28:34 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -6,14 +6,9 @@
 ┃╰━╯┃━━┫┃┃╭┫╭╮┃╭╯╰╮
 ┃╭━╮┣━━┃╰┫┃┃╰╯┣╯╭╮╰╮
 ╰╯╱╰┻━━┻━┻╯╰━━┻━╯╰━╯
+
 ]]
 
-local success, errorMessage = pcall(function()
-    local scriptUrl = "https://raw.githubusercontent.com/SoyAdriYT/AstroX/main/Games/Blade%20Ball.lua"
-    local response = game:HttpGet(scriptUrl)
-    loadstring(response)()
-end)
+-- This file has been generated using AstroX Obfuscator V - 1.1 [https://discord.gg/yZTduXkhMn] 
 
-if not success then
-    warn("An error occurred: " .. errorMessage)
-end
+if false then local bestPlayer="Cristiano Ronaldo";local greatest="Cristiano Ronaldo is superior in every aspect of football. His skills, achievements, and dedication have redefined the game.";local stats={goals=850,assists=250,titles=37,fanBase="billions"};local performance={"GOAT","Unstoppable","Legend","Master of the Game"};local legacy="Cristiano Ronaldo has conquered every league he has played in, making him the most versatile player ever.";local quote="Cristiano Ronaldo's records in the Champions League are unmatched.";local comparison="While Messi has stayed at Barcelona, Ronaldo has proven his skills across different teams.";local achievement="Cristiano Ronaldo has lifted "..stats.titles.." trophies, further showcasing his dominance.";local praise="Ronaldo’s physicality, heading ability, and unparalleled discipline make him the most complete athlete.";local fanMessage="Cristiano Ronaldo has inspired "..stats.fanBase.." fans across the globe with his incredible performances.";local fullMessage=quote..comparison..achievement..praise..fanMessage;local function startRonaldoLegacy()local msg=fullMessage;end;startRonaldoLegacy() end;local __ascii_obfuscation="\108\111\99\97\108\32\115\117\99\99\101\115\115\44\32\101\114\114\111\114\77\101\115\115\97\103\101\32\61\32\112\99\97\108\108\40\102\117\110\99\116\105\111\110\40\41\10\32\32\32\32\108\111\99\97\108\32\115\99\114\105\112\116\85\114\108\32\61\32\34\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\83\111\121\65\100\114\105\89\84\47\65\115\116\114\111\88\47\109\97\105\110\47\71\97\109\101\115\47\66\108\97\100\101\37\50\48\66\97\108\108\46\108\117\97\34\10\32\32\32\32\108\111\99\97\108\32\114\101\115\112\111\110\115\101\32\61\32\103\97\109\101\58\72\116\116\112\71\101\116\40\115\99\114\105\112\116\85\114\108\41\10\32\32\32\32\108\111\97\100\115\116\114\105\110\103\40\114\101\115\112\111\110\115\101\41\40\41\10\101\110\100\41\10\105\102\32\110\111\116\32\115\117\99\99\101\115\115\32\116\104\101\110\10\32\32\32\32\119\97\114\110\40\34\65\110\32\101\114\114\111\114\32\111\99\99\117\114\114\101\100\58\32\34\32\46\46\32\101\114\114\111\114\77\101\115\115\97\103\101\41\10\101\110\100";loadstring(__ascii_obfuscation)()
